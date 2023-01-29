7c32db72d2f247538b4104d2971e81909ff4d0a8
dmaxwell7 <desroymaxwell@gmail.com>
2023-01-29 01:43:33 -0600

    first commit

---
+++ src/Footer.js
@@ -0,0 +1,24 @@
+import React from 'react'
+// import InstagramIcon  from '@material-ui/icons/Instagram';
+// import TwitterIcon from '@material-ui/icons/Twitter';
+// import FacebookIcon from '@material-ui/icons/Facebook';
+// import LinkednIcon from '@material-ui/icons/LinkedIn';
+// import '../styles/Footer.css';
+
+
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <div className='socialmedia'>
+        {/* <FacebookIcon />
+        <InstagramIcon />
+        <LinkednIcon />
+        <TwitterIcon /> */}
+      </div>
+      <p> &copy; 2023 Crated by Desroy Maxwell </p>
+    </div>
+  )
+}
+
+export default Footer
